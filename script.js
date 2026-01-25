583c5c267292f9c19a2c00e1fbef9225ba272dd2
Nilakshi239 <nilakshibandara239@gmail.com>
2026-01-25 10:36:06 +0530

    arrange buttons

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
     // Toggle the mobile menu visibility
     document.body.classList.toggle("show-mobile-menu");
 });
+
+//close the menu when clicking the close button
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
